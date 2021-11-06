cc0681281dbaacbb491c9b3f7824398718f24f4d
gabrieldobre25 <42500193+gabrieldobre25@users.noreply.github.com>
2021-11-07 00:35:04 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,124 +1,114 @@
-import streamlit as st
+import streamlit as st 
 import streamlit.components.v1 as stc
+import requests 
+
+base_url = "https://jobs.github.com/positions.json?description={}&location={}"
+
+# Fxn to Retrieve Data
+def get_data(url):
+	resp = requests.get(url)
+	return resp.json()
 
 
-# Load EDA
-import pandas as pd
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.metrics.pairwise import cosine_similarity,linear_kernel
-
-
-# Load Our Dataset
-def load_data(data):
-	df = pd.read_csv(data)
-	return df
-
-
-# Fxn
-# Vectorize + Cosine Similarity Matrix
-
-def vectorize_text_to_cosine_mat(data):
-	count_vect = CountVectorizer()
-	cv_mat = count_vect.fit_transform(data)
-	# Get the cosine
-	cosine_sim_mat = cosine_similarity(cv_mat)
-	return cosine_sim_mat
-
-
-
-# Recommendation Sys
-@st.cache
-def get_recommendation(title,cosine_sim_mat,df,num_of_rec=10):
-	# indices of the course
-	course_indices = pd.Series(df.index,index=df['course_title']).drop_duplicates()
-	# Index of course
-	idx = course_indices[title]
-
-	# Look into the cosine matr for that index
-	sim_scores =list(enumerate(cosine_sim_mat[idx]))
-	sim_scores = sorted(sim_scores,key=lambda x: x[1],reverse=True)
-	selected_course_indices = [i[0] for i in sim_scores[1:]]
-	selected_course_scores = [i[0] for i in sim_scores[1:]]
-
-	# Get the dataframe & title
-	result_df = df.iloc[selected_course_indices]
-	result_df['similarity_score'] = selected_course_scores
-	final_recommended_courses = result_df[['course_title','similarity_score','url','price','num_subscribers']]
-	return final_recommended_courses.head(num_of_rec)
-
-
-RESULT_TEMP = """
-<div style="width:90%;height:100%;margin:1px;padding:5px;position:relative;border-radius:5px;border-bottom-right-radius: 60px;
-box-shadow:0 0 15px 5px #ccc; background-color: #a8f0c6;
-  border-left: 5px solid #6c6c6c;">
+JOB_HTML_TEMPLATE = """
+<div style="width:100%;height:100%;margin:1px;padding:5px;position:relative;border-radius:5px;border-bottom-right-radius: 10px;
+box-shadow:0 0 1px 1px #eee; background-color: #31333F;
+  border-left: 5px solid #6c6c6c;color:white;">
 <h4>{}</h4>
-<p style="color:blue;"><span style="color:black;">📈Score::</span>{}</p>
-<p style="color:blue;"><span style="color:black;">🔗</span><a href="{}",target="_blank">Link</a></p>
-<p style="color:blue;"><span style="color:black;">💲Price:</span>{}</p>
-<p style="color:blue;"><span style="color:black;">🧑‍🎓👨🏽‍🎓 Students:</span>{}</p>
+<h4>{}</h4>
+<h5>{}</h5>
+<h6>{}</h6>
 </div>
 """
 
-# Search For Course
-@st.cache
-def search_term_if_not_found(term,df):
-	result_df = df[df['course_title'].str.contains(term)]
-	return result_df
+JOB_DES_HTML_TEMPLATE = """
+<div style='color:#fff'>
+{}
+</div>
+"""
 
 
 def main():
-
-	st.title("Course Recommendation App")
-
-	menu = ["Home","Recommend","About"]
+	menu = ["Home","About"]
 	choice = st.sidebar.selectbox("Menu",menu)
 
-	df = load_data("data/udemy_course_data.csv")
+	st.title("DevDeeds -Search Jobs")
 
 	if choice == "Home":
 		st.subheader("Home")
-		st.dataframe(df.head(10))
+
+		# Nav  Search Form
+		with st.form(key='searchform'):
+			nav1,nav2,nav3 = st.beta_columns([3,2,1])
+
+			with nav1:
+				search_term = st.text_input("Search Job")
+			with nav2:
+				location = st.text_input("Location")
+
+			with nav3:
+				st.text("Search ")
+				submit_search = st.form_submit_button(label='Search')
+
+		st.success("You searched for {} in {}".format(search_term,location))
+
+		# Results
+		col1, col2 = st.beta_columns([2,1])
+
+		with col1:
+			if submit_search:
+				# Create Search Query
+				search_url = base_url.format(search_term,location)
+				# st.write(search_url)
+				data = get_data(search_url)
+
+				# Number of Results
+				num_of_results = len(data)
+				st.subheader("Showing {} jobs".format(num_of_results))
+				# st.write(data)
+		
+
+				for i in data:
+					job_title = i['title']
+					job_location = i['location']
+					company = i['company']
+					company_url = i['company_url']
+					job_post_date = i['created_at']
+					job_desc = i['description']
+					job_howtoapply = i['how_to_apply']
+					st.markdown(JOB_HTML_TEMPLATE.format(job_title,company,job_location,job_post_date),
+						unsafe_allow_html=True)
+
+					# Description
+					with st.beta_expander("Description"):
+						stc.html(JOB_DES_HTML_TEMPLATE.format(job_desc),scrolling=True)
+
+					# How to Apply
+					with st.beta_expander("How To Apply"):
+						# stc.html(job_howtoapply) # For White Theme
+						stc.html(JOB_DES_HTML_TEMPLATE.format(job_howtoapply),scrolling=True) # For Dark Theme
 
 
-	elif choice == "Recommend":
-		st.subheader("Recommend Courses")
-		cosine_sim_mat = vectorize_text_to_cosine_mat(df['course_title'])
-		search_term = st.text_input("Search")
-		num_of_rec = st.sidebar.number_input("Number",4,30,7)
-		if st.button("Recommend"):
-			if search_term is not None:
-				try:
-					results = get_recommendation(search_term,cosine_sim_mat,df,num_of_rec)
-					with st.beta_expander("Results as JSON"):
-						results_json = results.to_dict('index')
-						st.write(results_json)
+		with col2:
+			with st.form(key='email_form'):
+				st.write("Be the first to get new jobs info")
+				email = st.text_input("Email")
 
-					for row in results.iterrows():
-						rec_title = row[1][0]
-						rec_score = row[1][1]
-						rec_url = row[1][2]
-						rec_price = row[1][3]
-						rec_num_sub = row[1][4]
+				submit_email = st.form_submit_button(label='Subscribe')
 
-						# st.write("Title",rec_title,)
-						stc.html(RESULT_TEMP.format(rec_title,rec_score,rec_url,rec_url,rec_num_sub),height=350)
-				except:
-					results= "Not Found"
-					st.warning(results)
-					st.info("Suggested Options include")
-					result_df = search_term_if_not_found(search_term,df)
-					st.dataframe(result_df)
+				if submit_email:
+					st.success("A message was sent to {}".format(email))
 
 
 
-				# How To Maximize Your Profits Options Trading
 
 
 
 
 	else:
 		st.subheader("About")
-		st.text("Built with Streamlit & Pandas")
+
+
 
 
 if __name__ == '__main__':

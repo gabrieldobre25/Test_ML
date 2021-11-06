388f02746f22e3ea01c8974c5d184266fa86da9e
gabrieldobre25 <42500193+gabrieldobre25@users.noreply.github.com>
2021-11-07 00:07:24 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,60 +1,125 @@
-import pickle
-import streamlit as st
-import requests
-
-def fetch_poster(movie_id):
-    url = "https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US".format(movie_id)
-    data = requests.get(url)
-    data = data.json()
-    poster_path = data['poster_path']
-    full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
-    return full_path
-
-def recommend(movie):
-    index = movies[movies['title'] == movie].index[0]
-    distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
-    recommended_movie_names = []
-    recommended_movie_posters = []
-    for i in distances[1:6]:
-        # fetch the movie poster
-        movie_id = movies.iloc[i[0]].movie_id
-        recommended_movie_posters.append(fetch_poster(movie_id))
-        recommended_movie_names.append(movies.iloc[i[0]].title)
-
-    return recommended_movie_names,recommended_movie_posters
+import streamlit as st 
+import streamlit.components.v1 as stc 
 
 
-st.header('Movie Recommender System')
-movies = pickle.load(open('model/movie_list.pkl','rb'))
-similarity = pickle.load(open('model/similarity.pkl','rb'))
+# Load EDA
+import pandas as pd 
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity,linear_kernel
 
-movie_list = movies['title'].values
-selected_movie = st.selectbox(
-    "Type or select a movie from the dropdown",
-    movie_list
-)
 
-if st.button('Show Recommendation'):
-    recommended_movie_names,recommended_movie_posters = recommend(selected_movie)
-    col1, col2, col3, col4, col5 = st.beta_columns(5)
-    with col1:
-        st.text(recommended_movie_names[0])
-        st.image(recommended_movie_posters[0])
-    with col2:
-        st.text(recommended_movie_names[1])
-        st.image(recommended_movie_posters[1])
+# Load Our Dataset
+def load_data(data):
+	df = pd.read_csv(data)
+	return df 
 
-    with col3:
-        st.text(recommended_movie_names[2])
-        st.image(recommended_movie_posters[2])
-    with col4:
-        st.text(recommended_movie_names[3])
-        st.image(recommended_movie_posters[3])
-    with col5:
-        st.text(recommended_movie_names[4])
-        st.image(recommended_movie_posters[4])
+
+# Fxn
+# Vectorize + Cosine Similarity Matrix
+
+def vectorize_text_to_cosine_mat(data):
+	count_vect = CountVectorizer()
+	cv_mat = count_vect.fit_transform(data)
+	# Get the cosine
+	cosine_sim_mat = cosine_similarity(cv_mat)
+	return cosine_sim_mat
 
 
 
+# Recommendation Sys
+@st.cache
+def get_recommendation(title,cosine_sim_mat,df,num_of_rec=10):
+	# indices of the course
+	course_indices = pd.Series(df.index,index=df['course_title']).drop_duplicates()
+	# Index of course
+	idx = course_indices[title]
+
+	# Look into the cosine matr for that index
+	sim_scores =list(enumerate(cosine_sim_mat[idx]))
+	sim_scores = sorted(sim_scores,key=lambda x: x[1],reverse=True)
+	selected_course_indices = [i[0] for i in sim_scores[1:]]
+	selected_course_scores = [i[0] for i in sim_scores[1:]]
+
+	# Get the dataframe & title
+	result_df = df.iloc[selected_course_indices]
+	result_df['similarity_score'] = selected_course_scores
+	final_recommended_courses = result_df[['course_title','similarity_score','url','price','num_subscribers']]
+	return final_recommended_courses.head(num_of_rec)
 
 
+RESULT_TEMP = """
+<div style="width:90%;height:100%;margin:1px;padding:5px;position:relative;border-radius:5px;border-bottom-right-radius: 60px;
+box-shadow:0 0 15px 5px #ccc; background-color: #a8f0c6;
+  border-left: 5px solid #6c6c6c;">
+<h4>{}</h4>
+<p style="color:blue;"><span style="color:black;">📈Score::</span>{}</p>
+<p style="color:blue;"><span style="color:black;">🔗</span><a href="{}",target="_blank">Link</a></p>
+<p style="color:blue;"><span style="color:black;">💲Price:</span>{}</p>
+<p style="color:blue;"><span style="color:black;">🧑‍🎓👨🏽‍🎓 Students:</span>{}</p>
+</div>
+"""
+
+# Search For Course 
+@st.cache
+def search_term_if_not_found(term,df):
+	result_df = df[df['course_title'].str.contains(term)]
+	return result_df
+
+
+def main():
+
+	st.title("Course Recommendation App")
+
+	menu = ["Home","Recommend","About"]
+	choice = st.sidebar.selectbox("Menu",menu)
+
+	df = load_data("data/udemy_course_data.csv")
+
+	if choice == "Home":
+		st.subheader("Home")
+		st.dataframe(df.head(10))
+
+
+	elif choice == "Recommend":
+		st.subheader("Recommend Courses")
+		cosine_sim_mat = vectorize_text_to_cosine_mat(df['course_title'])
+		search_term = st.text_input("Search")
+		num_of_rec = st.sidebar.number_input("Number",4,30,7)
+		if st.button("Recommend"):
+			if search_term is not None:
+				try:
+					results = get_recommendation(search_term,cosine_sim_mat,df,num_of_rec)
+					with st.beta_expander("Results as JSON"):
+						results_json = results.to_dict('index')
+						st.write(results_json)
+
+					for row in results.iterrows():
+						rec_title = row[1][0]
+						rec_score = row[1][1]
+						rec_url = row[1][2]
+						rec_price = row[1][3]
+						rec_num_sub = row[1][4]
+
+						# st.write("Title",rec_title,)
+						stc.html(RESULT_TEMP.format(rec_title,rec_score,rec_url,rec_url,rec_num_sub),height=350)
+				except:
+					results= "Not Found"
+					st.warning(results)
+					st.info("Suggested Options include")
+					result_df = search_term_if_not_found(search_term,df)
+					st.dataframe(result_df)
+
+
+
+				# How To Maximize Your Profits Options Trading
+
+
+
+
+	else:
+		st.subheader("About")
+		st.text("Built with Streamlit & Pandas")
+
+
+if __name__ == '__main__':
+	main()
